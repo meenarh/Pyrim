548803530e0f10e0a846acbe7740a142377d3626
Aminat Ahmed <oahmedaminat@gmail.com>
2022-12-22 08:32:40 +0100

    added fixes to  FirstSection

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row ml-[30rem] mt-[3rem] gap-28">
     <img src="../assets/zara.png" />
     <img src="../assets/adidas.png" />
     <img src="../assets/gucci.png" />
