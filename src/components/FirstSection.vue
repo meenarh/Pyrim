93e6dd12b6fe2431f2c62ed57063dae7221a5fe8
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2022-12-22 08:34:21 +0100

    firstsection

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -2,6 +2,7 @@
     <div class="flex flex-col items-start relative">
         <img class="" src="../assets/FirstSection.png" />
         <div class="absolute h-[16.8125rem] w-[40.375rem] left-[100px] top-[231px] "><p class="font-normal not-italic text-[#371D1B] font-['Playfair_Display']">DISCOVER UNIQUE & AUTHENTIC WEARS</p></div>
+        
     </div>
 </template>
 
