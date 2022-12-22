8fa3519240718d3156897dadf8e4bc5f41508f91
Aminat Ahmed <oahmedaminat@gmail.com>
2022-12-22 19:45:16 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
 <div>
   <Header />
   <FirstSection />
+  <Footer />
 </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import FirstSection from './components/FirstSection'
+import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
     Header,
-    FirstSection
+    FirstSection,
+    Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-[#211212] w-[1440px] h-[408px] flex flex-col font-sans text-center pt-8">
+    <div class="p-4">
+      <h3 class="text-2xl text-white font-normal capitalize">
+        Subscribe to our newsletter
+      </h3>
+      <p class="text-sm text-[#F2ECE6] font-normal">
+        Get all the latest information on events, sales and offers.
+      </p>
+    </div>
+    <div class="p-4">
+      <input
+        class="p-3 bg-[#42302F] w-[492px] h-[60px] text-white font-normal text-sm]"
+        type="email"
+        placeholder="Enter your email"
+      />
+      <button class="bg-[#FEE9D8] w-[132px] h-[60px]" style="border-radius: 2px;">Subscribe</button>
+    </div>
+    <div>
+      <p class="text-sm text-[#908888] font-normal">
+        © 2022 Pyrim. All Rights Reserved.
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: `"Footer"`,
+};
+</script>
+<style lang=""></style>
